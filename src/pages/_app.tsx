66dc32ed6e058fa81845a180fa3ad4c6c1c602bd
Kenneth Kwakye-Gyamfi <kenneth.cross95@gmail.com>
2021-08-22 07:37:30 +0000

    feat: propagate analytics to potential pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { DefaultSeo, DefaultSeoProps } from 'next-seo';
 
 import { ThemeProvider } from '_/contexts/ThemeContext';
+import { pageView } from '_/utils/analytics';
 
 import '_/styles/global.css';
 
@@ -24,21 +26,38 @@ const DEFAULT_SEO: DefaultSeoProps = {
   },
 };
 
-const Cross19xx: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <meta
-        name="viewport"
-        content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-      />
-    </Head>
+const Cross19xx: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
+  const { events: routerEvents } = router;
 
-    <DefaultSeo {...DEFAULT_SEO} />
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageView(url);
+    };
 
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </>
-);
+    routerEvents.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      routerEvents.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [routerEvents]);
+
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
+
+      <DefaultSeo {...DEFAULT_SEO} />
+
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default Cross19xx;
