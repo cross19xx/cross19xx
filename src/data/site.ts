723d50087c4bc46d86987cda7a2778e937b58ce0
Kenneth Kwakye-Gyamfi <kenneth.cross95@gmail.com>
2026-06-13 13:19:54 +0000

    setup footer

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,1 +1,9 @@
 export const EMAIL = 'kenneth.cross95@gmail.com';
+
+export const SOCIAL_MEDIA = [
+  { label: 'GitHub', href: 'https://www.github.com/cross19xx' },
+  { label: 'LinkedIn', href: 'https://www.linkedin.com/in/kenneth-kwakye-gyamfi' },
+  { label: 'X', href: 'https://twitter.com/cross19xx' },
+  { label: 'Instagram', href: 'https://www.instagram.com/kenneth_kwakyegyamfi' },
+  { label: 'Facebook', href: 'https://www.facebook.com/profile.php?id=100009436144652' },
+];
