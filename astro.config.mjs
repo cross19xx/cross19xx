3e68f42064cea5a73789d52d0335aa6c49968196
Kenneth Kwakye-Gyamfi <kenneth.cross95@gmail.com>
2026-06-15 10:43:15 +0000

    setup vercel adapter for SSR conversion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, fontProviders } from 'astro/config';
 
 export default defineConfig({
+  adapter: vercel(),
   fonts: [
     {
       provider: fontProviders.local(),
